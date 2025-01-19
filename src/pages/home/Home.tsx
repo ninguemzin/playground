a5392f276955d02c8d4ba8e9ab25a19dc9029dd8
ninguemzin <alm1427@hotmail.com>
2025-01-19 17:54:49 -0300

    login

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,7 @@
       <div className="bg-indigo-900 flex justify-center">
         <div className="container grid grid-cols-2 text-white">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
+            <h2 className="text-5xl font-bold">Seja Bem Vindo!</h2>
             <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
